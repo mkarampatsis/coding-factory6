06d0e02e12073a768d122e08576834003c4665dd
mkarampatsis <marka@central.ntua.gr>
2024-10-21 18:54:40 +0300

    angular material

--- angular/example-3/src/app/components/component-output-example/component-output-example.component.ts
+++ angular/example-3/src/app/components/component-output-example/component-output-example.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
-import { EPerson } from '../../shared/interfaces/person';
+import { EPerson, Person } from '../../shared/interfaces/person';
+import {
+    Dialog,
+    DialogRef,
+    DIALOG_DATA,
+    DialogModule
+} from '@angular/cdk/dialog';
+import { PersonTableComponent } from '../person-table/person-table.component';
 
 @Component({
   selector: 'app-component-output-example',
   standalone: true,
-  imports: [SimpleDatatableComponent],
+  imports: [SimpleDatatableComponent, DialogModule],
   templateUrl: './component-output-example.component.html',
   styleUrl: './component-output-example.component.css'
 })
+
 export class ComponentOutputExampleComponent {
     
+    constructor (public dialog: Dialog) {}
+
     showPersonClicked(person: EPerson){
-        alert(this.personTemplate(person))
+        // alert(this.personTemplate(person))
+        this.dialog.open(PersonDialogComponent, {
+            data: person
+        })
     }
 
     personTemplate(person: EPerson){
@@ -26,4 +39,30 @@ export class ComponentOutputExampleComponent {
         Address: ${person.address}
         `
     }
+}
+
+@Component({
+    imports: [PersonTableComponent],
+    standalone: true,
+    template: `
+        <app-person-table [person]="person"></app-person-table>
+        <button class="btn btn-primary btn-sm" (click)="dialogRef.close()">Close</button>         
+    `,
+    styles: [
+        `
+            :host {
+                display: block;
+                background: #fff;
+                border-radius:8px;
+                padding: 16px;
+                max-width: 500px;
+            }
+        `,
+    ],
+})
+export class PersonDialogComponent {
+    constructor(
+        public dialogRef: DialogRef,
+        @Inject(DIALOG_DATA) public person: Person,
+    ){}
 }
