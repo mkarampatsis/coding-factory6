670709367feebc45648dc6fe40cb45a1b111d9c8
mkarampatsis <marka@central.ntua.gr>
2024-10-22 20:49:13 +0300

    angular

--- angular/example-3/src/app/components/template-driven-forms/template-driven-forms.component.ts
+++ angular/example-3/src/app/components/template-driven-forms/template-driven-forms.component.ts
@@ -1,23 +1,15 @@
 import { Component } from '@angular/core';
-import { TemplateFormComponent } from './template-form/template-form.component';
-import { EPerson, ManyPerson } from '../../shared/interfaces/person';
-import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
 import { PersonTableComponent } from '../person-table/person-table.component';
+import { SimpleDatatableComponent } from '../simple-datatable/simple-datatable.component';
+import { TemplateFormStructureComponent } from './template-form-structure/template-form-structure.component';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
-  imports: [TemplateFormComponent, SimpleDatatableComponent, PersonTableComponent],
+  imports: [PersonTableComponent, SimpleDatatableComponent, TemplateFormStructureComponent],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.css'
 })
 export class TemplateDrivenFormsComponent {
-    currentPerson: EPerson | undefined;
-    
-    onPerson(person: EPerson){
-        console.log("From Parent>>>",person);
-        this.currentPerson = person;
-        ManyPerson.push(person);
-        console.log("CurrentPerson>>",this.currentPerson);
-    }
+
 }
---
+++ angular/example-3/src/app/components/template-driven-forms/template-form-structure/template-form-structure.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-form-structure',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './template-form-structure.component.html',
+  styleUrl: './template-form-structure.component.css'
+})
+export class TemplateFormStructureComponent {
+
+}
